f7769403d70dfc736cbd7d6dfcb5c517d9268444
Yashar Honarmandi <yashar@honarmandi.se>
2026-09-21 17:24:43 +0200

    Add MQTT topic list to settings

--- app/settings/app.py
+++ app/settings/app.py
@@ -3,7 +3,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class AppSettings(BaseSettings):
-    model_config = SettingsConfigDict()
+    # Seems to be necessary to ignore here in order for topics list to be correctly parsed
+    model_config = SettingsConfigDict(extra="ignore")
 
     mqtt_host: str = Field()
     mqtt_port: int = Field(default=1883)
+    mqtt_topics: list[str] = Field()
